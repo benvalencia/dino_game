18c425471b577ec74996e8273378ca7b094e4fa9
Ben <benyamin.ak@hotmail.com>
2025-10-19 14:10:02 +0200

    feat(#3): Crear variables de elementos

--- script/script.js
+++ script/script.js
@@ -3,8 +3,13 @@ const context = dino_game.getContext("2d");
 
 const WORLD_WIDTH = 800;
 const WORLD_HEIGHT = 200;
+const DINO_WIDTH = 88 / 1.5;
+const DINO_HEIGHT = 94 / 1.5;
+const MAX_JUMP_HEIGHT = WORLD_HEIGHT;
+const MIN_JUMP_HEIGHT = 150;
 
 let scaleRatio = null;
+let previousTime = null;  
 
 function setScreen() {
   scaleRatio = getScaleRatio();
@@ -39,3 +44,23 @@ function getScaleRatio() {
   }
   
 }
+
+function clearWorld() { 
+  context.fillStyle = "#fff";
+  context.fillRect(0, 0, dino_game.width, dino_game.height);
+}
+
+function worldLoop(currentTime) {
+  if (previousTime === null) {
+    previousTime = currentTime;
+    requestAnimationFrame(worldLoop)
+    return;
+  }
+  const frameRate = currentTime - previousTime;
+  previousTime = currentTime;
+  
+  clearWorld();
+  requestAnimationFrame(worldLoop);
+}
+
+requestAnimationFrame(worldLoop);
